82bae1b7bf936ecd35fed2728b8fcc4e59d851dc
cediperez12 <34096750+cediperez12@users.noreply.github.com>
2020-01-26 20:01:32 +0800

    Incomplete Register

--- app/src/main/java/com/coecs/project012/Register.java
+++ app/src/main/java/com/coecs/project012/Register.java
@@ -1,18 +1,30 @@
 package com.coecs.project012;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import de.hdodenhof.circleimageview.CircleImageView;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageActivity;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import org.w3c.dom.Text;
 
@@ -23,10 +35,10 @@ public class Register extends AppCompatActivity{
     private FirebaseDatabase database;
 
     private TextInputLayout til_first_name,til_last_name,til_email,til_password,til_confirm_pass;
-
     private EditText etxt_fname,etxt_lname,etxt_email,etxt_password,etxt_confirm_pass;
-
     private CircleImageView civ_profile_image;
+
+    private Uri profileImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -112,13 +124,155 @@ public class Register extends AppCompatActivity{
 
             @Override
             public void afterTextChanged(Editable s) {
+                try{
+                    if(isValid(s.toString()))
+                        throw new Exception("Your email address is invalid.");
+                    if(s.toString().isEmpty())
+                        throw new Exception("Please enter your email.");
+                    til_email.setErrorEnabled(false);
+                }catch(Exception ex){
+                    til_email.setErrorEnabled(true);
+                    til_email.setError(ex.getMessage());
+                }
+            }
+        });
+        etxt_password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                try{
+                    if(s.toString().isEmpty())
+                        throw new Exception("Please enter your password");
+                    if(s.toString().length() < 8)
+                        throw new Exception("Your password must be 8 and above");
+                    til_password.setErrorEnabled(false);
+                }catch (Exception ex){
+                    til_password.setErrorEnabled(true);
+                    til_password.setError(ex.getMessage());
+                }
+            }
+        });
+        etxt_confirm_pass.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                try{
+                    if(s.toString().isEmpty())
+                        throw new Exception("Please confirm your password");
+                    if(!s.toString().equals(etxt_password.getText().toString()))
+                        throw new Exception("Your password does not matched");
+                    til_confirm_pass.setErrorEnabled(false);
+                }catch(Exception ex){
+                    til_confirm_pass.setErrorEnabled(true);
+                    til_confirm_pass.setError(ex.getMessage());
+                }
             }
         });
     }
 
     public void clickRegister(View view){
+        try{
+            if(til_first_name.isErrorEnabled())
+                throw new Exception(til_first_name.getError().toString());
+            if(til_last_name.isErrorEnabled())
+                throw new Exception(til_last_name.getError().toString());
+            if(til_password.isErrorEnabled())
+                throw new Exception(til_password.getError().toString());
+            if(til_confirm_pass.isErrorEnabled())
+                throw new Exception(til_confirm_pass.getError().toString());
 
+            final String email = til_email.getEditText().getText().toString();
+            final String password = til_password.getEditText().getText().toString();
+
+            authentication.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        String uid = task.getResult().getUser().getUid();
+                    }else{
+
+                    }
+                }
+            });
+
+        }catch(Exception ex){
+            new AlertDialog.Builder(this)
+                    .setTitle("Error")
+                    .setMessage(ex.getMessage())
+                    .setPositiveButton("OKAY",null)
+                    .setCancelable(false)
+                    .create().show();
+        }
+    }
+
+    public void clickChooseImage(View view){
+        CropImage.activity()
+                .setAspectRatio(1,1)
+                .setFixAspectRatio(true)
+                .setActivityTitle("Crop your profile image")
+                .setAllowFlipping(false)
+                .setAllowRotation(false)
+                .setCropShape(CropImageView.CropShape.OVAL)
+                .start(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                profileImage = result.getUri();
+                civ_profile_image.setImageURI(profileImage);
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception ex = result.getError();
+            }
+        }
+    }
+
+    private boolean isValid(String email) {
+        String regex = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
+        return email.matches(regex);
+    }
+
+    private class RegisterFunction extends AsyncTask<String,String,Void> {
+
+        RegisterFunction(){
+
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(String... strings) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
     }
 }
 
---
+++ app/src/main/java/com/coecs/project012/User.java
@@ -0,0 +1,135 @@
+package com.coecs.project012;
+
+public class User {
+    private String email;
+    private String password;
+
+    private String firstName;
+    private String lastName;
+
+    private String uid;
+
+    private String profileImagePath;
+
+    private boolean workerMode;
+
+    private EducationalAttainment[] educations;
+    private Experiences[] experiences;
+    private String[] skills;
+    private Location userLocation;
+
+    public User(String email, String password, String firstName, String lastName, String uid, String profileImagePath) {
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.uid = uid;
+        this.profileImagePath = profileImagePath;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getProfileImagePath() {
+        return profileImagePath;
+    }
+
+    public void setProfileImagePath(String profileImagePath) {
+        this.profileImagePath = profileImagePath;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public boolean isWorkerMode() {
+        return workerMode;
+    }
+
+    public void setWorkerMode(boolean workerMode) {
+        this.workerMode = workerMode;
+    }
+
+    public EducationalAttainment[] getEducations() {
+        return educations;
+    }
+
+    public void setEducations(EducationalAttainment[] educations) {
+        this.educations = educations;
+    }
+
+    public Experiences[] getExperiences() {
+        return experiences;
+    }
+
+    public void setExperiences(Experiences[] experiences) {
+        this.experiences = experiences;
+    }
+
+    public String[] getSkills() {
+        return skills;
+    }
+
+    public void setSkills(String[] skills) {
+        this.skills = skills;
+    }
+
+    public Location getUserLocation() {
+        return userLocation;
+    }
+
+    public void setUserLocation(Location userLocation) {
+        this.userLocation = userLocation;
+    }
+
+    private class Location{
+        private long lat;
+        private long lng;
+    }
+
+    private class EducationalAttainment{
+        private String school;
+        private String course;
+        private String attainment;
+        private String year;
+    }
+
+    private class Experiences{
+        private String years;
+        private String company;
+        private String jobTitle;
+    }
+}
