c8e3a463a358d09d704de4deb0406eb5a1cfef5c
cediperez12 <34096750+cediperez12@users.noreply.github.com>
2020-01-29 16:31:50 +0800

    Added Main Activity Navigation Drawer

--- app/src/main/java/com/coecs/project012/MainActivity.java
+++ app/src/main/java/com/coecs/project012/MainActivity.java
@@ -1,14 +1,21 @@
 package com.coecs.project012;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
     }
 }
