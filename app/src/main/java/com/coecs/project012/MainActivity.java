0b10ebbb20db7f26c621eae55710a1bb1c22c840
cediperez12 <34096750+cediperez12@users.noreply.github.com>
2020-01-29 17:03:49 +0800

    Added Main Activity Navigation Drawer

--- app/src/main/java/com/coecs/project012/MainActivity.java
+++ app/src/main/java/com/coecs/project012/MainActivity.java
@@ -1,11 +1,15 @@
 package com.coecs.project012;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawer;
 
     private Toolbar toolbar;
 
@@ -17,5 +21,20 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar = findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar, R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
     }
 }
