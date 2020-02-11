22ba929e71c260538c870b4717880926ecd89ce2
cediperez12 <34096750+cediperez12@users.noreply.github.com>
2020-02-11 21:56:49 +0800

    Error at WorkerSetup [View Pager] Fixed MainActivity Profile At NavigationView

---
+++ app/src/main/java/com/coecs/project012/WorkerSetupEducation.java
@@ -0,0 +1,31 @@
+package com.coecs.project012;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WorkerSetupEducation extends Fragment {
+
+
+    public WorkerSetupEducation() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_worker_setup_education, container, false);
+    }
+
+}
